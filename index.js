2b73ee9ffaf117f9aab9da040f2e5b11e9066708
Vitalik-D <vitalikdyaduchko@gmail.com>
2019-04-09 17:53:01 +0300

    added curson pointer, added css

--- index.js
+++ index.js
@@ -6,99 +6,97 @@ const index = express();
 const jsonParser = bodyParser.json();
 
 index.use(express.static(__dirname + "/public"));
-index.get("/api/users", function(req, res){
-    const content = fs.readFileSync("users.json", "utf8");
-    const users = JSON.parse(content);
-    res.send(users);
+index.get("/api/users", function(req, res) {
+  const content = fs.readFileSync("users.json", "utf8");
+  const users = JSON.parse(content);
+  res.send(users);
 });
-index.get("/api/users/:id", function(req, res){
-
-    let id = req.params.id; // получаем id
-    const content = fs.readFileSync("users.json", "utf8");
-    let users = JSON.parse(content);
-    let user = null;
-    for(let i=0; i<users.length; i++){
-        if(users[i].id==id){
-            user = users[i];
-            break;
-        }
+index.get("/api/users/:id", function(req, res) {
+  let id = req.params.id; // получаем id
+  const content = fs.readFileSync("users.json", "utf8");
+  let users = JSON.parse(content);
+  let user = null;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id == id) {
+      user = users[i];
+      break;
     }
-    if(user){
-        res.send(user);
-    }
-    else{
-        res.status(404).send();
-    }
+  }
+  if (user) {
+    res.send(user);
+  } else {
+    res.status(404).send();
+  }
 });
-index.post("/api/users", jsonParser, function (req, res) {
+index.post("/api/users", jsonParser, function(req, res) {
+  if (!req.body) return res.sendStatus(400);
 
-    if(!req.body) return res.sendStatus(400);
+  const userName = req.body.name;
+  const userAge = req.body.age;
+  const user = { name: userName, age: userAge };
 
-    const userName = req.body.name;
-    const userAge = req.body.age;
-    const user = {name: userName, age: userAge};
+  let data = fs.readFileSync("users.json", "utf8");
+  const users = JSON.parse(data);
 
-    let data = fs.readFileSync("users.json", "utf8");
-    const users = JSON.parse(data);
-
-    const id = Math.max.apply(Math,users.map(function(o){return o.id;}))
-    user.id = id+1;
-    users.push(user);
-    data = JSON.stringify(users);
+  const id = Math.max.apply(
+    Math,
+    users.map(function(o) {
+      return o.id;
+    })
+  );
+  user.id = id + 1;
+  users.push(user);
+  data = JSON.stringify(users);
+  fs.writeFileSync("users.json", data);
+  res.send(user);
+});
+index.delete("/api/users/:id", function(req, res) {
+  const id = req.params.id;
+  const data = fs.readFileSync("users.json", "utf8");
+  const users = JSON.parse(data);
+  let index = -1;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id == id) {
+      index = i;
+      break;
+    }
+  }
+  if (index > -1) {
+    const user = users.splice(index, 1)[0];
+    const data = JSON.stringify(users);
     fs.writeFileSync("users.json", data);
     res.send(user);
+  } else {
+    res.status(404).send();
+  }
 });
-index.delete("/api/users/:id", function(req, res){
+index.put("/api/users", jsonParser, function(req, res) {
+  if (!req.body) return res.sendStatus(400);
 
-    const id = req.params.id;
-    const data = fs.readFileSync("users.json", "utf8");
-    const users = JSON.parse(data);
-    let index = -1;
-    for(let i=0; i<users.length; i++){
-        if(users[i].id==id){
-            index=i;
-            break;
-        }
-    }
-    if(index > -1){
-        const user = users.splice(index, 1)[0];
-        const data = JSON.stringify(users);
-        fs.writeFileSync("users.json", data);
-        res.send(user);
-    }
-    else{
-        res.status(404).send();
-    }
-});
-index.put("/api/users", jsonParser, function(req, res){
+  const userId = req.body.id;
+  const userName = req.body.name;
+  const userAge = req.body.age;
 
-    if(!req.body) return res.sendStatus(400);
-
-    const userId = req.body.id;
-    const userName = req.body.name;
-    const userAge = req.body.age;
-
-    const data = fs.readFileSync("users.json", "utf8");
-    let users = JSON.parse(data);
-    let user;
-    for(let i=0; i<users.length; i++){
-        if(users[i].id==userId){
-            user = users[i];
-            break;
-        }
+  const data = fs.readFileSync("users.json", "utf8");
+  let users = JSON.parse(data);
+  let user;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id == userId) {
+      user = users[i];
+      break;
     }
-    if(user){
-        user.age = userAge;
-        user.name = userName;
-        const data = JSON.stringify(users);
-        fs.writeFileSync("users.json", data);
-        res.send(user);
-    }
-    else{
-        res.status(404).send(user);
-    }
+  }
+  if (user) {
+    user.age = userAge;
+    user.name = userName;
+    const data = JSON.stringify(users);
+    fs.writeFileSync("users.json", data);
+    res.send(user);
+  } else {
+    res.status(404).send(user);
+  }
 });
 
-index.listen(port, function(){
-    console.log("Loading...");
+index.listen(port, function() {
+  console.log("Loading...");
 });
--- public/app.js
+++ public/app.js
@@ -1,113 +1,123 @@
 function GetUsers() {
-    $.ajax({
-        url: "/api/users",
-        type: "GET",
-        contentType: "application/json",
-        success: function (users) {
-            var rows = "";
-            $.each(users, function (index, user) {
-                rows += row(user);
-            })
-            $("table tbody").append(rows);
-        }
-    });
+  $.ajax({
+    url: "/api/users",
+    type: "GET",
+    contentType: "application/json",
+    success: function(users) {
+      var rows = "";
+      $.each(users, function(index, user) {
+        rows += row(user);
+      });
+      $("table tbody").append(rows);
+    }
+  });
 }
 function GetUser(id) {
-    $.ajax({
-        url: "/api/users/"+id,
-        type: "GET",
-        contentType: "application/json",
-        success: function (user) {
-            var form = document.forms["userForm"];
-            form.elements["id"].value = user.id;
-            form.elements["name"].value = user.name;
-            form.elements["age"].value = user.age;
-        }
-    });
+  $.ajax({
+    url: "/api/users/" + id,
+    type: "GET",
+    contentType: "application/json",
+    success: function(user) {
+      var form = document.forms["userForm"];
+      form.elements["id"].value = user.id;
+      form.elements["name"].value = user.name;
+      form.elements["age"].value = user.age;
+    }
+  });
 }
 function CreateUser(userName, userAge) {
-    $.ajax({
-        url: "api/users",
-        contentType: "application/json",
-        method: "POST",
-        data: JSON.stringify({
-            name: userName,
-            age: userAge
-        }),
-        success: function (user) {
-            reset();
-            $("table tbody").append(row(user));
-        }
-    })
+  $.ajax({
+    url: "api/users",
+    contentType: "application/json",
+    method: "POST",
+    data: JSON.stringify({
+      name: userName,
+      age: userAge
+    }),
+    success: function(user) {
+      reset();
+      $("table tbody").append(row(user));
+    }
+  });
 }
 function EditUser(userId, userName, userAge) {
-    $.ajax({
-        url: "api/users",
-        contentType: "application/json",
-        method: "PUT",
-        data: JSON.stringify({
-            id: userId,
-            name: userName,
-            age: userAge
-        }),
-        success: function (user) {
-            reset();
-            $("tr[data-rowid='" + user.id + "']").replaceWith(row(user));
-        }
-    })
+  $.ajax({
+    url: "api/users",
+    contentType: "application/json",
+    method: "PUT",
+    data: JSON.stringify({
+      id: userId,
+      name: userName,
+      age: userAge
+    }),
+    success: function(user) {
+      reset();
+      $("tr[data-rowid='" + user.id + "']").replaceWith(row(user));
+    }
+  });
 }
 
 function reset() {
-    var form = document.forms["userForm"];
-    form.reset();
-    form.elements["id"].value = 0;
+  var form = document.forms["userForm"];
+  form.reset();
+  form.elements["id"].value = 0;
 }
 
 function DeleteUser(id) {
-    $.ajax({
-        url: "api/users/"+id,
-        contentType: "application/json",
-        method: "DELETE",
-        success: function (user) {
-            console.log(user);
-            $("tr[data-rowid='" + user.id + "']").remove();
-        }
-    })
+  $.ajax({
+    url: "api/users/" + id,
+    contentType: "application/json",
+    method: "DELETE",
+    success: function(user) {
+      console.log(user);
+      $("tr[data-rowid='" + user.id + "']").remove();
+    }
+  });
 }
 
-var row = function (user) {
-    return "<tr data-rowid='" + user.id + "'><td>" + user.id + "</td>" +
-        "<td>" + user.name + "</td> <td>" + user.age + "</td>" +
-        "<td><a class='editLink' data-id='" + user.id + "'>Edit</a> | " +
-        "<a class='removeLink' data-id='" + user.id + "'>Remove</a></td></tr>";
-}
+var row = function(user) {
+  return (
+    "<tr data-rowid='" +
+    user.id +
+    "'><td>" +
+    user.id +
+    "</td>" +
+    "<td>" +
+    user.name +
+    "</td> <td>" +
+    user.age +
+    "</td>" +
+    "<td><a class='editLink' data-id='" +
+    user.id +
+    "'>Edit</a> | " +
+    "<a class='removeLink' data-id='" +
+    user.id +
+    "'>Remove</a></td></tr>"
+  );
+};
 
-$("#reset").click(function (e) {
-
-    e.preventDefault();
-    reset();
-})
-
-
-$("form").submit(function (e) {
-    e.preventDefault();
-    var id = this.elements["id"].value;
-    var name = this.elements["name"].value;
-    var age = this.elements["age"].value;
-    if (id == 0)
-        CreateUser(name, age);
-    else
-        EditUser(id, name, age);
+$("#reset").click(function(e) {
+  e.preventDefault();
+  reset();
 });
 
-$("body").on("click", ".editLink", function () {
-    var id = $(this).data("id");
-    GetUser(id);
-})
+$("form").submit(function(e) {
+  e.preventDefault();
+  var id = this.elements["id"].value;
+  var name = this.elements["name"].value;
+  var age = this.elements["age"].value;
+  if (id == 0) CreateUser(name, age);
+  else EditUser(id, name, age);
+});
 
-$("body").on("click", ".removeLink", function () {
-    var id = $(this).data("id");
-    DeleteUser(id);
-})
+$("body").on("click", ".editLink", function() {
+  var id = $(this).data("id");
+  GetUser(id);
+});
+
+$("body").on("click", ".removeLink", function() {
+  var id = $(this).data("id");
+  DeleteUser(id);
+});
 
 GetUsers();
